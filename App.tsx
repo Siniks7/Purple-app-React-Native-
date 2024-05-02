662e09be4e66dfa8ae2872d8be36f5375dba5996
Nikolay <siniks7@yandex.ru>
2024-05-02 21:16:59 +0700

    first exercise

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Dimensions, Image, StyleSheet, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const width = Dimensions.get('window').width;
+  const height = Dimensions.get('window').height;
+
+  const styles = StyleSheet.create({
+    root: {
+      backgroundColor: 'black',
+      height: height,
+      width: width,   
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    container: {
+      minWidth: 350,
+      gap: 10
+    },
+    textStyle: { color: 'blue', fontSize: 24, borderWidth: 1, borderColor: 'red' },
+    myInput: { backgroundColor: 'grey', height: 50, color: 'white', borderRadius: 10, paddingLeft: 20},
+    myButton1: { backgroundColor: 'grey', height: 50 },
+    myButton2: { backgroundColor: 'grey', height: 50 }
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={styles.root}>
+      <View style={styles.container}>
+                <Image></Image>
+          <TextInput placeholder='Email' style={styles.myInput}></TextInput>
+          <TextInput  placeholder='Пароль' style={styles.myInput}></TextInput>
+          <Button title='Войти'></Button>
+          <Button title='Восстановить пароль'></Button>
+            </View >
+            </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+

13636ee864e148d0c096ed00935d4274a2dce3aa
Nikolay <siniks7@yandex.ru>
2024-05-10 18:20:45 +0700

    stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,30 @@
 import { Stack } from 'expo-router';
+import { Colors } from '../shared/tokens';
 
 export default function RootRayout() {
-	return <Stack />;
+	return (
+		<Stack
+			screenOptions={{
+				statusBarColor: Colors.black,
+				contentStyle: {
+					backgroundColor: Colors.black,
+				},
+			}}
+		>
+			<Stack.Screen
+				name="index"
+				options={{
+					presentation: 'modal',
+					headerShown: false,
+				}}
+			/>
+			<Stack.Screen
+				name="restore"
+				options={{
+					presentation: 'modal',
+					headerShown: false,
+				}}
+			/>
+		</Stack>
+	);
 }

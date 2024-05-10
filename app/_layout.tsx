e4216175324b674caa2a69a1bf661d2621d51b25
Nikolay <siniks7@yandex.ru>
2024-05-10 20:24:46 +0700

    SafeArea и StatusBar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,30 +1,36 @@
 import { Stack } from 'expo-router';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Colors } from '../shared/tokens';
 
 export default function RootRayout() {
+	const insets = useSafeAreaInsets();
 	return (
-		<Stack
-			screenOptions={{
-				statusBarColor: Colors.black,
-				contentStyle: {
-					backgroundColor: Colors.black,
-				},
-			}}
-		>
-			<Stack.Screen
-				name="index"
-				options={{
-					presentation: 'modal',
-					headerShown: false,
+		<SafeAreaProvider>
+			{/* <StatusBar style="light" /> */}
+			<Stack
+				screenOptions={{
+					statusBarColor: Colors.black,
+					contentStyle: {
+						backgroundColor: Colors.black,
+						paddingTop: insets.top,
+					},
 				}}
-			/>
-			<Stack.Screen
-				name="restore"
-				options={{
-					presentation: 'modal',
-					headerShown: false,
-				}}
-			/>
-		</Stack>
+			>
+				<Stack.Screen
+					name="index"
+					options={{
+						presentation: 'modal',
+						headerShown: false,
+					}}
+				/>
+				<Stack.Screen
+					name="restore"
+					options={{
+						presentation: 'modal',
+						headerShown: false,
+					}}
+				/>
+			</Stack>
+		</SafeAreaProvider>
 	);
 }
--- app/restore.tsx
+++ app/restore.tsx
@@ -1,11 +1,12 @@
 import { Link } from 'expo-router';
 import { Text, View } from 'react-native';
+import { Colors } from '../shared/tokens';
 
 export default function Restore() {
 	return (
 		<View>
 			<Link href={'/'}>
-				<Text>Restore</Text>
+				<Text style={{ color: Colors.white }}>Restore</Text>
 			</Link>
 		</View>
 	);

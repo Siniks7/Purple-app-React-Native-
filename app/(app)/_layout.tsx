f80d2d5214dc3e3fa77fb052f3c4e7369de64c42
Nikolay <siniks7@yandex.ru>
2024-05-25 11:03:13 +0700

    asyncStorage

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,9 +1,29 @@
 import { Stack } from 'expo-router';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Colors } from '../../shared/tokens';
 
 export default function AppRayout() {
+	const insets = useSafeAreaInsets();
+
 	return (
-		<Stack>
-			<Stack.Screen name="index" />
-		</Stack>
+		<SafeAreaProvider>
+			<Stack
+				screenOptions={{
+					statusBarColor: Colors.black,
+					contentStyle: {
+						backgroundColor: Colors.black,
+						paddingTop: insets.top,
+					},
+				}}
+			>
+				<Stack.Screen
+					name="index"
+					options={{
+						presentation: 'modal',
+						headerShown: false,
+					}}
+				/>
+			</Stack>
+		</SafeAreaProvider>
 	);
 }
--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,7 +1,18 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect } from 'react';
 import { Text, View } from 'react-native';
 import { Colors } from '../../shared/tokens';
 
 export default function MyCourses() {
+	useEffect(() => {
+		AsyncStorage.setItem('demo', 'test').then(async () => {
+			console.log(await AsyncStorage.getAllKeys());
+			console.log(await AsyncStorage.getItem('demo'));
+			console.log(await AsyncStorage.removeItem('demo'));
+			console.log(await AsyncStorage.getItem('demo'));
+		});
+	}, []);
+
 	return (
 		<View>
 			<Text style={{ color: Colors.white }}>MyCourses</Text>

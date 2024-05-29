d082c3a24b539e7a45495eec68b1f467b0227a6a
Nikolay <siniks7@yandex.ru>
2024-05-29 22:51:44 +0700

    Редирект

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,9 +1,15 @@
-import { Stack } from 'expo-router';
+import { Redirect, Stack } from 'expo-router';
+import { useAtomValue } from 'jotai';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { authAtom } from '../../entities/auth/model/auth.state';
 import { Colors } from '../../shared/tokens';
 
 export default function AppRayout() {
 	const insets = useSafeAreaInsets();
+	const { access_token } = useAtomValue(authAtom);
+	if (!access_token) {
+		return <Redirect href="/login" />;
+	}
 
 	return (
 		<SafeAreaProvider>
--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,22 +1,7 @@
-import { router, useRootNavigationState } from 'expo-router';
-import { useAtomValue } from 'jotai';
-import { useEffect } from 'react';
 import { Text, View } from 'react-native';
-import { authAtom } from '../../entities/auth/model/auth.state';
 import { Colors } from '../../shared/tokens';
 
 export default function MyCourses() {
-	// const [auth, login] = useAtom(loginAtom);
-	// const logout = useSetAtom(logoutAtom);
-	const { access_token } = useAtomValue(authAtom);
-	const state = useRootNavigationState();
-	useEffect(() => {
-		if (!state?.key) return;
-		if (!access_token) {
-			router.replace('/login');
-		}
-	}, [access_token, state]);
-
 	return (
 		<View>
 			<Text style={{ color: Colors.white }}>index</Text>

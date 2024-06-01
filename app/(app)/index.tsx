88be94894047666dd448b0777d64e7bf54f59c52
Nikolay <siniks7@yandex.ru>
2024-06-01 12:04:24 +0700

    Реализация логина и индикатор загрузки

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,10 +1,15 @@
+import { useSetAtom } from 'jotai';
 import { Text, View } from 'react-native';
+import { logoutAtom } from '../../entities/auth/model/auth.state';
+import { Button } from '../../shared/Button/Button';
 import { Colors } from '../../shared/tokens';
 
 export default function MyCourses() {
+	const logout = useSetAtom(logoutAtom);
 	return (
 		<View>
 			<Text style={{ color: Colors.white }}>index</Text>
+			<Button text="Выход" onPress={logout} />
 		</View>
 	);
 }
--- app/login.tsx
+++ app/login.tsx
@@ -1,5 +1,8 @@
-import { useState } from 'react';
+import { router } from 'expo-router';
+import { useAtom } from 'jotai';
+import { useEffect, useState } from 'react';
 import { Image, StyleSheet, View } from 'react-native';
+import { loginAtom } from '../entities/auth/model/auth.state';
 import { Button } from '../shared/Button/Button';
 import { CustomLink } from '../shared/CustomLink/CustomLink';
 import { ErrorNotification } from '../shared/ErrorNotification/ErrorNotification';
@@ -7,14 +10,35 @@ import { Input } from '../shared/Input/Input';
 import { Colors, Gaps } from '../shared/tokens';
 
 export default function App() {
-	const [error, setError] = useState<string | undefined>();
+	const [localError, setLocalError] = useState<string | undefined>();
+	const [email, setEmail] = useState<string>();
+	const [password, setPassword] = useState<string>();
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [{ access_token, isLoading, error }, login] = useAtom(loginAtom);
 
-	const alert = () => {
-		setError('Неверный логин и пароль');
-		setTimeout(() => {
-			setError(undefined);
-		}, 4000);
+	const submit = () => {
+		if (!email) {
+			setLocalError('Не введён email');
+			return;
+		}
+		if (!password) {
+			setLocalError('Не введён пароль');
+			return;
+		}
+		login({ email, password });
 	};
+
+	useEffect(() => {
+		if (error) {
+			setLocalError(error);
+		}
+	}, [error]);
+
+	useEffect(() => {
+		if (access_token) {
+			router.replace('/');
+		}
+	}, [access_token]);
 
 	const styles = StyleSheet.create({
 		container: {
@@ -38,15 +62,15 @@ export default function App() {
 
 	return (
 		<View style={styles.container}>
-			<ErrorNotification error={error} />
+			<ErrorNotification error={localError} />
 			<View style={styles.content}>
 				<Image style={styles.logo} source={require('../assets/logo.png')} resizeMode="contain" />
 				<View style={styles.form}>
-					<Input placeholder="Email" />
-					<Input isPassword placeholder="Пароль" />
-					<Button text="Войти" onPress={alert} />
+					<Input placeholder="Email" onChangeText={setEmail} />
+					<Input isPassword placeholder="Пароль" onChangeText={setPassword} />
+					<Button text="Войти" onPress={submit} isLoading={isLoading} />
 				</View>
-				<CustomLink href={'/course/typescript'} text="Восстановить пароль" />
+				<CustomLink href={'/restore'} text="Восстановить пароль" />
 			</View>
 		</View>
 	);

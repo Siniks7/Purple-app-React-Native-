68a2cd06f210524f06241ed705d9a01785b27614
Nikolay <siniks7@yandex.ru>
2024-07-07 11:20:47 +0700

    finish

--- shared/api.ts
+++ shared/api.ts
@@ -1,5 +1,5 @@
-export const PREFIX = `${process.env.EXPO_PUBLIC_DOMAIN}/api-v2`;
-
+// export const PREFIX = `${process.env.EXPO_PUBLIC_DOMAIN}/api-v2`;
+export const PREFIX = 'https://purpleschool.ru/api-v2';
 export const FILE_API = {
 	uploadImage: `${PREFIX}/files/upload-image?folder=demo`,
 };
